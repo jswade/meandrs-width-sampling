0f5961598ec243cbfadd7152f1afcfaaf9778c91
Jeffrey Wade <jeffwade200@gmail.com>
2024-10-09 15:26:30 -0700

    VIC test v3

--- tst/tst_pub_repr_Wade_etal_202xb.sh
+++ tst/tst_pub_repr_Wade_etal_202xb.sh
@@ -35,8 +35,8 @@ rank='10'
 #*****************************************************************************
 #Retrieve number of files preset in output directories
 #*****************************************************************************
-num_files=$(ls ../input/MeanDRS/Qout_COR/* | wc -l)
-
+#num_files=$(ls ../input/MeanDRS/Qout_COR/* | wc -l)
+num_files=$(ls ../input/MeanDRS/riv_UNCOR/* | wc -l)
 
 #*****************************************************************************
 #Select which unit tests to perform based on inputs to this shell script
